cf91be28fb5a9d4c1c79c278219e8f7b7d19bd70
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2017-07-28 00:12:26 +0200

    Code for exit buttons

---
+++ UI/SMUIManager.cs
@@ -0,0 +1,41 @@
+﻿//
+//  SMUIManager.cs
+//  Jultar
+//
+//  Created by Sandeep Yadav Mattepu on 27/07/17.
+//  Copyright © 2017 Mattepu. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace SandeepMattepu.UI
+{
+	public class SMUIManager : MonoBehaviour 
+	{
+		public RectTransform areYouSureExitPanel;
+
+		void Start()
+		{
+			areYouSureExitPanel.gameObject.SetActive (false);
+		}
+
+		public void exitButtonPressed()
+		{
+			areYouSureExitPanel.gameObject.SetActive (true);
+		}
+
+		public void yesSureToExitPressed(string sceneName)
+		{
+			SceneManager.LoadScene (sceneName);
+		}
+
+		public void noSureToExitPressed()
+		{
+			areYouSureExitPanel.gameObject.SetActive (false);
+		}
+	}	
+}
