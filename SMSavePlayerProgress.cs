87f6324694aa7dc8e256682f6c4eafad68f2ff74
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2017-08-11 12:59:54 +0200

    Save player progress

---
+++ SMSavePlayerProgress.cs
@@ -0,0 +1,116 @@
+﻿//
+//  SMSavePlayerProgress.cs
+//  Jultar
+//
+//  Created by Sandeep Yadav Mattepu on 10/08/17.
+//  Copyright © 2017 Mattepu. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SandeepMattepu.Multiplayer;
+
+namespace SandeepMattepu
+{
+	/// <summary>
+	/// This class loads and saves the player progress made in the game
+	/// </summary>
+	public class SMSavePlayerProgress : MonoBehaviour 
+	{
+		/// <summary>
+		/// The killes made by player in his entire game life.
+		/// </summary>
+		private int killesMade = UserManager.Stats[(int)Stat.Kills];
+		/// <summary>
+		/// The deaths encountered by player in his entire game life..
+		/// </summary>
+		private int deathsEncountered = UserManager.Stats[(int)Stat.Deaths];
+		/// <summary>
+		/// The time played by the player in his entire game time.
+		/// </summary>
+		private int timePlayed = UserManager.Stats[(int)Stat.TimePlayed];
+		/// <summary>
+		/// The total wins made by player.
+		/// </summary>
+		private int totalWinsMadeByPlayer = UserManager.Stats[(int)Stat.Won];
+		/// <summary>
+		/// The total loses made by player.
+		/// </summary>
+		private int totalLosesMadeByPlayer = UserManager.Stats[(int)Stat.Lost];
+
+		// Use this for initialization
+		void Start () 
+		{
+			SMMultiplayerGame.OnGameOver += savePlayerData;
+		}
+
+		void OnDestroy()
+		{
+			SMMultiplayerGame.OnGameOver -= savePlayerData;
+		}
+
+		/// <summary>
+		/// This function is called at the end of the game to save player progress
+		/// </summary>
+		private void savePlayerData()
+		{
+			int localPlayerId = PhotonNetwork.player.ID;
+			int deathsNumber = 0;
+			int scoreMade;
+			bool isDataReadyToSave = false;
+
+			timePlayed += (int)SMMultiplayerGame.INSTANCE.gameSessionTime;
+			if(SMMultiplayerGame.PlayerIdAndDeaths.TryGetValue (localPlayerId, out deathsNumber))
+			{
+				deathsEncountered += deathsNumber;
+			}
+
+			if(SMMultiplayerGame.gameType == MPGameTypes.FREE_FOR_ALL)
+			{
+				if(SMFreeForAll.IsLocalPlayerLeading)
+				{
+					totalWinsMadeByPlayer += 1;
+				}
+				else
+				{
+					totalLosesMadeByPlayer += 1;
+				}
+
+				if(SMFreeForAll.PlayersIdAndScore.TryGetValue(localPlayerId, out scoreMade))
+				{
+					killesMade += scoreMade;
+					isDataReadyToSave = true;
+				}
+			}
+			else if(SMMultiplayerGame.gameType == MPGameTypes.TEAM_DEATH_MATCH)
+			{
+				if(SMTeamDeathMatch.IsLocalPlayerTeamLeading)
+				{
+					totalWinsMadeByPlayer += 1;
+				}
+				else
+				{
+					totalLosesMadeByPlayer += 1;
+				}
+
+				if(SMTeamDeathMatch.PlayersIdAndScore.TryGetValue(localPlayerId, out scoreMade))
+				{
+					killesMade += scoreMade;
+					isDataReadyToSave = true;
+				}
+			}
+
+			if(isDataReadyToSave)
+			{
+				UserManager.Stats [(int)Stat.Kills] = killesMade;
+				UserManager.Stats [(int)Stat.Deaths] = deathsEncountered;
+				UserManager.Stats [(int)Stat.TimePlayed] = timePlayed;
+				UserManager.Stats [(int)Stat.Won] = totalWinsMadeByPlayer;
+				UserManager.Stats [(int)Stat.Lost] = totalLosesMadeByPlayer;
+				UserManager.SaveCloud ();
+			}
+
+		}
+	}
+}
