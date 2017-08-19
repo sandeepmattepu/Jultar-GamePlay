d1035ac18408d89915ac507cae5dbc61f1134d59
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2017-08-19 16:58:13 +0200

    Xp boost product will persist in game

--- Multiplayer/SMPlayerSpawnerAndAssigner.cs
+++ Multiplayer/SMPlayerSpawnerAndAssigner.cs
@@ -418,5 +418,6 @@ public class SMPlayerSpawnerAndAssigner : MonoBehaviour
 			}
 		}
 		specialWeaponManager.registerMultiplayerGame(gameRulesThatSpawned);
+		gameRulesThatSpawned.xpMadeAfterKill *= SMProductEquipper.INSTANCE.CurrentExpBoostMultiplier;
 	}
 }
