e0f0669499db4d57e57f98f7ad73ea6660910259
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2017-09-02 15:56:53 +0200

    Custom debug code for testing on phones

---
+++ Android/SMCustomDebug.cs
@@ -0,0 +1,53 @@
+﻿//
+//  SMCustomDebug.cs
+//  Jultar
+//
+//  Created by Sandeep Yadav Mattepu on 02/09/17.
+//  Copyright © 2017 Mattepu. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SandeepMattepu.Testing
+{
+	/// <summary>
+	/// This class helps us to show debug messages with custom tags during testing on phone
+	/// </summary>
+	public class SMCustomDebug
+	{
+		private static string DEFAULT_TAG = "UNITY_SANDEEP";
+
+		#if UNITY_ANDROID
+		/// <summary>
+		/// This holds reference to android java class which shows custom debug messages
+		/// </summary>
+		private static AndroidJavaClass androidCustomDebugClass = new AndroidJavaClass("com.mattepu.sandeep_jultar_plugin.DebugWithCustomTags");
+		#endif
+
+		/// <summary>
+		/// Shows the debug message in android monitor or in xcode debugger.
+		/// </summary>
+		/// <param name="message">Message to be shown.</param>
+		private static void showDebugMessage(string message)
+		{
+			#if UNITY_ANDROID
+			androidCustomDebugClass.CallStatic("showCustomLogWithTag",DEFAULT_TAG,message);
+			#endif
+		}
+
+		/// <summary>
+		/// Shows the debug message in android monitor or in xcode debugger.
+		/// </summary>
+		/// <param name="tag">Tag to filter in debugger.</param>
+		/// <param name="message">Message to be shown.</param>
+		private static void showDebugMessage(string tag, string message)
+		{
+			#if UNITY_ANDROID
+			androidCustomDebugClass.CallStatic("showCustomLogWithTag",tag,message);
+			#endif
+		}
+	}
+}
+
