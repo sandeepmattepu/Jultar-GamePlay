cf054706c13b06b8c502d9ec78436984d312c6a1
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MacBook-Pro.local>
2017-09-29 00:19:51 +0200

    News feed about game

---
+++ Web/SMNewsFeed.cs
@@ -0,0 +1,59 @@
+﻿//
+//  SMNewsFeed.cs
+//  Jultar
+//
+//  Created by Sandeep Yadav Mattepu on 28/09/17.
+//  Copyright © 2017 Mattepu. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SandeepMattepu.Web
+{
+	/// <summary>
+	/// This class loads the server for news feed which will be displayed to player
+	/// </summary>
+	public class SMNewsFeed : MonoBehaviour
+	{
+		/// <summary>
+		/// The user interface to display news feed.
+		/// </summary>
+		[SerializeField]
+		private Text uiToDisplayNewsFeed;
+		/// <summary>
+		/// The web address of news feed.
+		/// </summary>
+		public string webAddressOfNewsFeed = "http://mattepu.com/NewsFeed.php";
+
+		void Start () 
+		{
+			retriveDataFromServer ();
+		}
+
+		/// <summary>
+		/// This function retrives the data from server.
+		/// </summary>
+		public void retriveDataFromServer()
+		{
+			StartCoroutine ("waitAndLoadData");
+		}
+
+		private IEnumerator waitAndLoadData()
+		{
+			WWW request = new WWW (webAddressOfNewsFeed);
+			yield return request;
+			Debug.Log ("Data Ready");
+			if(uiToDisplayNewsFeed != null)
+			{
+				uiToDisplayNewsFeed.text = request.text;
+			}
+			else
+			{
+				Debug.Log (request.text);
+			}
+		}
+	}
+}
