674e2e3c20141e62897e1764a3e1d19a7518f354
Sandeep Yadav Mattepu <sandeepyadavmattepu@Sandeeps-MBP.local>
2017-10-12 02:02:37 +0200

    Game version added for photon servers

---
+++ Multiplayer/SMPhotonGameVersion.cs
@@ -0,0 +1,29 @@
+﻿//
+//  SMPhotonGameVersion.cs
+//  Jultar
+//
+//  Created by Sandeep Yadav Mattepu on 12/10/17.
+//  Copyright © 2017 Mattepu. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+namespace SandeepMattepu.Multiplayer
+{
+	/// <summary>
+	/// This class assigns game version for photon servers.
+	/// </summary>
+	public class SMPhotonGameVersion : MonoBehaviour 
+	{
+		[SerializeField]
+		private ObscuredString gameVersion = "1.0";
+
+		private void Start () 
+		{
+			PhotonNetwork.gameVersion = gameVersion;
+		}
+	}
+}
